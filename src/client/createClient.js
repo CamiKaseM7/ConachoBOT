9600ca6f0c0744d51b2b6489572efd432d70f1c7
CamiKaze <camilorivasramunni@gmail.com>
2021-10-08 21:05:27 -0300

    Anonymous Connection

--- src/client/createClient.js
+++ src/client/createClient.js
@@ -4,10 +4,10 @@ require("dotenv").config({ path: "./src/credentials/.env" });
 function createClient(channels) {
 	const config = {
 		options: { debug: process.env.DEBUG == "true", joinInterval: 500 },
-		identity: {
-			username: process.env.BOT_USERNAME,
-			password: process.env.BOT_TOKEN,
-		},
+		// identity: {
+		// 	username: process.env.BOT_USERNAME,
+		// 	password: process.env.BOT_TOKEN,
+		// },
 		channels: channels,
 	};
 	const client = tmi.Client(config);
